a919cfa6a725494a8813fa20f6868b96f206052c
lipusipu44 <anilpatro044@gmail.com>
2025-01-03 12:35:53 +0530

    Push post tmpl changes and router handler modification

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -48,3 +48,24 @@ func (m *Repository)About(w http.ResponseWriter, r *http.Request){
 	
 	render.TemplateRender(w,"about.page.tmpl",&tempData)
 }
+
+func (m *Repository)Reservation(w http.ResponseWriter, r *http.Request){
+	render.TemplateRender(w,"make-reservation.page.tmpl",&tempData)
+}
+
+func (m *Repository)Generals(w http.ResponseWriter, r *http.Request){
+	render.TemplateRender(w,"generals.page.tmpl",&tempData)
+}
+
+func (m *Repository)Majors(w http.ResponseWriter, r *http.Request){
+	render.TemplateRender(w,"majors.page.tmpl",&tempData)
+}
+
+func (m *Repository)Availability(w http.ResponseWriter, r *http.Request){
+	render.TemplateRender(w,"search-availability.page.tmpl",&tempData)
+}
+
+func (m *Repository)Contact(w http.ResponseWriter,r *http.Request){
+	render.TemplateRender(w,"contact.page.tmpl",&tempData)
+}
+
